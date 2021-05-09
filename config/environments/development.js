7f8bcf0ed89fbf5e31988ad977ba5705f3271d8c
Grigori Grudinin <grgr14@mail.ru>
2021-05-10 00:45:35 +0300

    add db

--- config/environments/development.js
+++ config/environments/development.js
@@ -1,16 +1,11 @@
 /* @ts-check */
 /* eslint-disable no-param-reassign */
 
-import nodosWebpack from '@nodosjs/webpack-extension';
+import nodosWebpack from "@nodosjs/webpack-extension";
 
 export default async (app) => {
   app.addExtension(nodosWebpack);
 
-  app.config.logLevel = 'debug';
+  app.config.logLevel = "debug";
   app.config.cacheModules = false;
-
-  app.config.db = {
-    type: 'sqljs',
-    location: 'db/development.sqlite',
-  };
 };
